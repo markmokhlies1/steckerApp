a59b3fe3f4b262cef1a61f15409486105d955250
markmokhlies1 <165431456+markmokhlies1@users.noreply.github.com>
2024-12-23 19:52:49 +0200

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Stecker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
